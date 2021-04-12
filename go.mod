1f71c8cd2ba9f8275bab0462294fb8042df19f0b
Ankush.K <ankush.khobragade@aquasec.com>
2021-04-12 10:54:12 +0530

    added hash module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ankk13/multimodgo
 
 go 1.16
+
+require github.com/ankk13/gohash v0.0.0-20210412050630-bf213dd06a27 // indirect
--- multimodgo.go
+++ multimodgo.go
@@ -2,12 +2,22 @@ package multimodgo
 
 import (
 	"fmt"
+
+	"github.com/ankk13/gohash"
 )
 
-// Hello
 // Hello world funct
 func Hello() string {
 	str := "Hello, World."
 	fmt.Println(str)
 	return str
+}
+
+// HelloHash returns hash of helloWorld
+func HelloHash() string {
+	str := "Hello, World."
+	client := gohash.Client{}
+	hashedValue := client.SHA256(str)
+	fmt.Println(hashedValue)
+	return hashedValue
 }
